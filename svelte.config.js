b0e49e81c4ac3e00a44bc97addad0f5457dcdb07
Camryn Truban <camtruban@gmail.com>
2025-03-27 23:49:21 -0400

    try diff' adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,30 @@
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+
+
+// const config = {
+// 	preprocess: vitePreprocess(),
+// 	kit: {
+// 		paths: {
+// 			base: dev ? '' : process.env.BASE_URL || '/'
+// 		},
+// 		adapter: adapter()
+// 	}
+// }
+// export default config;
+
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
-
-const config = {
-	preprocess: vitePreprocess(),
+export default {
 	kit: {
-		paths: {
-			base: dev ? '' : process.env.BASE_URL || '/'
-		},
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
-}
-export default config;
+};
