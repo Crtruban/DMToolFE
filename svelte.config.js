a48538093083e4d590d448f2101325c2550aaefa
Camryn Truban <camtruban@gmail.com>
2025-03-27 23:45:31 -0400

    svelte config changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
+
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			base: dev ? '' : process.env.BASE_URL || '/'
+		},
+		adapter: adapter()
 	}
-};
-
+}
 export default config;
